ab0fee302ba3621593ec052a656ce7ee5aac37db
Rich Lim <richardlim92@gmail.com>
2020-02-17 21:39:33 -0500

    Added background image to splash, changed up some styling

--- frontend/components/home/sidebar.jsx
+++ frontend/components/home/sidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ChannelIndex from '../channels/channel_index_container';
+import { Link } from 'react-router-dom';
 
 class SideBar extends React.Component {
     constructor(props) {
@@ -9,7 +10,12 @@ class SideBar extends React.Component {
     render() {
         return (
             <div className="home-container sidebar">
-                <h1 className="workplace">SIDEBAR</h1>
+                <h1 className="workplace"><Link className="logo" to="/home">
+                    <span className="red">m</span>
+                    <span className="blue">e</span>
+                    <span className="yellow">e</span>
+                    <span className="green">p</span>
+                </Link></h1>
                 <h3 className="sidebar-username">{this.props.user.username}</h3>
                 <button className="logout" onClick={this.props.logout}>Log out</button>
                 <ChannelIndex />
