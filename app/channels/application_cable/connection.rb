84761424d8ad91df415b02011106c59467a7c7ba
Rich Lim <richardlim92@gmail.com>
2020-02-25 13:35:16 -0500

    Testing config for heroku live chat

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -15,5 +15,23 @@ module ApplicationCable
     def find_verified_user
       verified_user = User.find_by(session_token: request.session.fetch('session_token', nil))
     end
+
+
+    # identified_by :current_user
+
+    # def connect
+    #   self.current_user = find_verified_user
+    #   logger.add_tags 'ActionCable', current_user.name
+    # end
+
+    # protected
+    #   def find_verified_user
+    #     verified_user = User.find_by(id: cookies.signed['user.id'])
+    #     if verified_user && cookies.signed['user.expires_at'] > Time.now
+    #       verified_user
+    #     else
+    #       reject_unauthorized_connection
+    #     end
+    #   end
   end
 end
