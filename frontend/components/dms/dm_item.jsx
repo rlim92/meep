ff79af91f79aa87a335c9e8c76b72e79762b0e8a
Rich Lim <richardlim92@gmail.com>
2020-03-01 18:24:27 -0500

    Add prof pics to create dm form

--- frontend/components/dms/dm_item.jsx
+++ frontend/components/dms/dm_item.jsx
@@ -24,10 +24,12 @@ class DmItem extends React.Component {
     render() {
         const { dm, currentUserId, users } = this.props;
         let name = [];
+        let userId;
 
         if (dm) {
             for (let i = 0; i < dm.member_ids.length; i++) {
                 if (dm.member_ids[i] !== currentUserId) {
+                    userId = dm.member_ids[i];
                     name.push(users[dm.member_ids[i]].username);
                 }
             }
@@ -35,12 +37,162 @@ class DmItem extends React.Component {
             name.push(users.username);
         }
 
+        let img;
+
+        if (users[userId]) {
+            if (users[userId].username === "DemoUser") {
+                img = (
+                    <img
+                        className="profile-icon"
+                        src={blackIcon}
+                        width="36"
+                        height="36"
+                    />
+                )
+            } else if (users[userId].username === 'rich') {
+                img = (
+                    <img
+                        className="profile-icon"
+                        src={richIcon}
+                        width="36"
+                        height="36"
+                    />
+                )
+            } else {
+                switch (userId % 5) {
+                    case 0:
+                        img = (
+                            <img
+                                className="profile-icon"
+                                src={blackIcon}
+                                width="36"
+                                height="36"
+                            />
+                        )
+                        break;
+                    case 1:
+                        img = (
+                            <img
+                                className="profile-icon"
+                                src={redIcon}
+                                width="36"
+                                height="36"
+                            />
+                        )
+                        break;
+                    case 2:
+                        img = (
+                            <img
+                                className="profile-icon"
+                                src={blueIcon}
+                                width="36"
+                                height="36"
+                            />
+                        )
+                        break;
+                    case 3:
+                        img = (
+                            <img
+                                className="profile-icon"
+                                src={yellowIcon}
+                                width="36"
+                                height="36"
+                            />
+                        )
+                        break;
+                    case 4:
+                        img = (
+                            <img
+                                className="profile-icon"
+                                src={greenIcon}
+                                width="36"
+                                height="36"
+                            />
+                        )
+                        break;
+                }
+            }
+        } else if (users.id) {
+            if (users.username === "DemoUser") {
+                img = (
+                    <img
+                        className="profile-icon"
+                        src={blackIcon}
+                        width="36"
+                        height="36"
+                    />
+                )
+            } else if (users.username === 'rich') {
+                img = (
+                    <img
+                        className="profile-icon"
+                        src={richIcon}
+                        width="36"
+                        height="36"
+                    />
+                )
+            } else {
+                switch (users.id % 5) {
+                    case 0:
+                        img = (
+                            <img
+                                className="profile-icon"
+                                src={blackIcon}
+                                width="36"
+                                height="36"
+                            />
+                        )
+                        break;
+                    case 1:
+                        img = (
+                            <img
+                                className="profile-icon"
+                                src={redIcon}
+                                width="36"
+                                height="36"
+                            />
+                        )
+                        break;
+                    case 2:
+                        img = (
+                            <img
+                                className="profile-icon"
+                                src={blueIcon}
+                                width="36"
+                                height="36"
+                            />
+                        )
+                        break;
+                    case 3:
+                        img = (
+                            <img
+                                className="profile-icon"
+                                src={yellowIcon}
+                                width="36"
+                                height="36"
+                            />
+                        )
+                        break;
+                    case 4:
+                        img = (
+                            <img
+                                className="profile-icon"
+                                src={greenIcon}
+                                width="36"
+                                height="36"
+                            />
+                        )
+                        break;
+                }
+            }
+        }
         
         return (
             <li
-                className="pub-li"
+                className="pub-li dm-create-item"
                 onClick={this.handleClick}>
-                <h3 className="pub-channel-name dm-item">{name.join(", ")}</h3>
+                {img}
+                <h3 className="pub-channel-name dm-item dm-create-name-item">{name.join(", ")}</h3>
             </li>
         )
     }
